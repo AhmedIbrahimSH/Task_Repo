e8d1dc938423307084ade15dbbb9e5edd875f830
ahmedibrahim132002@gmail.com <ahmedibrahim132002@gmail.com>
2023-11-03 17:29:25 +0200

    added min to main

--- main.cpp
+++ main.cpp
@@ -11,11 +11,21 @@ int getSum(vector<int>& numbers){
     return sum;
 }
 
+int getMin(vector<int>& numbers){
+    int min = numbers[0];
+    for(int itr = 0 ; i < numbers.size() ; i++){
+        if(numbers[itr] < min){
+            min = numbers[itr];
+        }
+    }
+    return min;
+}
+
 
 int main(){
 
     vector<int>& numbers = {1,2,3,4,5,6,7,8,9,10};
     int sum = getSum(numbers);
     cout << "Sum of numbers is: " << sum << endl;
-
+    cout << "Min of numbers is: " << getMin(numbers) << endl;
 }
