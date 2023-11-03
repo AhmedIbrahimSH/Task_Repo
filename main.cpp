273c30810afeca5fc944d53a1658ba67121c3245
ahmedibrahim132002@gmail.com <ahmedibrahim132002@gmail.com>
2023-11-03 17:27:48 +0200

    added average to main.cpp

--- main.cpp
+++ main.cpp
@@ -11,11 +11,17 @@ int getSum(vector<int>& numbers){
     return sum;
 }
 
+int getAverage(vector<int>& numbers){
+    int sum = getSum(numbers);
+    int average = sum / numbers.size();
+    return average;
+}
+
 
 int main(){
 
     vector<int>& numbers = {1,2,3,4,5,6,7,8,9,10};
     int sum = getSum(numbers);
     cout << "Sum of numbers is: " << sum << endl;
-
+    cout << "Average of numbers is: " << getAverage(numbers) << endl;
 }
